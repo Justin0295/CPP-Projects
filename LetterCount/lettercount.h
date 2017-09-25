c00c26de0c9f3e55a7d26a72838a32e44289b657
G-Moncrief <30476251+G-Moncrief@users.noreply.github.com>
2017-09-25 17:43:55 -0400

    Create lettercount.h

---
+++ LetterCount/lettercount.h
@@ -0,0 +1,15 @@
+#ifndef __LetterCount_H
+#define __LetterCount_H
+
+#include <string>
+using namespace std;
+
+class LetterCount
+{
+	private:
+		int* counter;
+	public:
+		void print() const;
+};
+
+#endif
