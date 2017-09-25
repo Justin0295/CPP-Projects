8a564f2e0388f1102ae7e0c42fcd67c0b274ac11
G-Moncrief <30476251+G-Moncrief@users.noreply.github.com>
2017-09-24 23:24:43 -0400

    Update iomanipTest.cpp

--- iomanipTest.cpp
+++ iomanipTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 #include <stdlib.h>
 using namespace std;
 
@@ -7,20 +8,32 @@ int main()
 {
 	//prompts user for number of shares and price until an invalid input is entered
 
-	double numshares, price, total(0);
+	vector <double> prices, numshares;
+	double shares, price, total(0);
 
 	cout << "Enter the number of shares and price per share. Enter Q to quit: ";
-	cin >> numshares >> price;
+	cin >> shares >> price;
 
 	while (!cin.fail())
 	{
-	total += numshares * price;
-	cout << "Enter the number of shares and price per share: ";
-	cin >> numshares >> price;
+		numshares.push_back(shares);
+		prices.push_back(price);
+		total += shares * price;
+		cout << "Enter the number of shares and price per share. Enter Q to quit: ";
+		cin >> shares >> price;
+		if (shares <= 0 || price <= 0)
+			break;
 	}
 
-	cout << '$' << fixed << setprecision(2) << total << endl;
+	cout << fixed;
+	for (int i = 0; i < numshares.size(); i++)
+		cout << setw(5) << setprecision(1) << numshares[i] << " shares at $"
+		<< setw(6) << setprecision(2) << prices[i] << " = $"
+		<< setw(8) << numshares[i] * prices[i] << endl;
 
+	cout << "=========\n"
+		<< "     Total: $";
+	cout << setprecision(2) << total << endl;
 	system("PAUSE");
 
 	return 0;
