dcfb8c7143ad8994c333537acd533785af04cdfa
G-Moncrief <30476251+G-Moncrief@users.noreply.github.com>
2017-11-29 09:18:48 -0500

    Add files via upload

---
+++ OOD-Final/link.h
@@ -0,0 +1,22 @@
+# ifndef __LINK_H
+#define __LINK_H
+
+class Link
+{
+	private:
+		string link_disp;
+		string link_addr;
+	public:
+		Link() {cout << "Link demo running"; }
+		Link(const string disp, const string addr)
+		{
+			link_disp = disp;
+			link_addr=addr;
+		}
+		string print() { return link_disp; }
+		string go() {return link_addr; }
+};
+
+
+
+#endif
