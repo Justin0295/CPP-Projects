f7a7d9cd3ad96f15ff694c5b40ddb31d58cac2b1
G-Moncrief <30476251+G-Moncrief@users.noreply.github.com>
2017-11-29 05:46:55 -0500

    Revising formatting and some functions

--- OOD-Final/final.cpp
+++ OOD-Final/final.cpp
@@ -4,66 +4,97 @@
 #include <fstream>
 
 using namespace std;
-void delve(string s);
 
-main()
+bool end_of_story = 0;
+
+void foo();
+string delve(const string s, const string title);
+
+main ()
 {
-	ifstream pFile("if.html");
-	stringstream buf;
-	
-	//these will eventually be part of the Interpreter class but for now they are here
-	string pass_name;
-	string pass_text;
-	
-	//delimiters
-	string pass_start = "<tw-passagedata";
-	string pass_end = "</tw-passagedata>";
-	string link_start = "[[";
-	string link_end = "]]";
-	string pass_name_is = "name=\"";
+	ifstream pFile ("if.html");
 	
 	if (pFile.is_open())
 	{
-		cout << "\n\nOpening file....\n\n";
-		
-		//convert pFile to string---idk if will need this 
+		cout << "\n\nOPENING FILE....\n" << endl;
+		stringstream buf;
 		buf << pFile.rdbuf();
 		string story = buf.str();
+		string pass_name = "start";
 		
-		//display the first passage
+		//find first passage title
+		size_t start_check_1 = story.find("=\"New Game");
+		size_t start_check_2 = story.find("=\"Start");
+		size_t start_check_3 = story.find("=\"start");
+		size_t start_check_4 = story.find("=\"new game");
+		if (start_check_1 != std::string::npos)
+			pass_name = ("New Game");
+		else if (start_check_2 != std::string::npos)
+			pass_name = ("Start");
+		else if (start_check_3 != std::string::npos)
+			pass_name = ("start");
+		else if (start_check_4 != std::string::npos)
+			pass_name = ("new game");
+		else
+			cout << "Cannot determine start" << endl; //temp will need to replace with another way to find start
 		
-		//passage name - - gets the passage name
-		pass_name = "start"; //for testing to be removed
-		unsigned beg_pos_name= story.find("name=\"");
-		cout << story.substr (beg_pos_name+6, pass_name.length()) << endl;
-		
-		//display passage - - finds passage of x name and builds substring...will pass this to a function to get links etc
-		pass_name = "start"; //for testing to be removed
-		unsigned beg_pos_pass = story.find("name=\"" +pass_name + "\"");  //sets pos of passage with name="x"
-		string s = story.substr(beg_pos_pass, 2000);
-		unsigned end_pos_twtag = s.find(">");  //sets pos of the end of the <tw> tag for selected passage
-		unsigned end_of_pass = s.find("</tw");  //sets pos of the end of the passage
-		
-		//cout << s.substr (end_pos_twtag+1, end_of_pass - end_pos_twtag-1) <<endl; //for testing to be removed
-		delve ( s.substr (end_pos_twtag+1, end_of_pass - end_pos_twtag-1));
-		
-		//cout << story;   //for testing to be removed
-		
-		cout << "\n";
+		//loop that runs story
+		while (end_of_story != 1)
+		{
+			delve (story, pass_name);
+			foo();
+		}
 	}
+	
 	else
 	{
 		cout << "cannot find file";
 	}
 	
-	
-	
 	return 0;
 }
 
-//delve is the function that will pick out all the variables and links
-void delve(string s)
+//goes through selected passage picking out display text links sets etc
+string delve (const string s, const string title)
 {
+	size_t find_passage = s.find("\""+title+"\""); //finds the pos of the segment in HTML file
 	
-	cout << s;
+	bool end_of_header = false;
+	
+	for (int i = find_passage; i < s.length() ; )
+	{
+		if (end_of_header == false && s[i] != '>') //ignores header information per assignment instructions
+			i++;
+		if (end_of_header == false && s[i] == '>') //ignores header information per assignment instructions
+		{
+			end_of_header = true;
+			i++;
+		}
+		if (s[i] == '[') //possible link...checks if [[ format...if not prints char and moves on
+		{
+			if (s[i+1] == '[')
+			{
+				cout << "LINK FOUND" << endl;
+			}
+			else
+			{
+				cout << s[i];
+				i++;
+			}
+		}
+		if (end_of_header == true && s[i] != '<') //if text is not part of a link set or header it will be disp
+		{
+			cout << s[i];
+			i++;
+		}
+		if (s[i] == '<') //ends when reaches the end of the passage
+			break;
+		
+	}
+	
+	return "foo";
+}
+void foo ()
+{
+	end_of_story =1;
 }
